8aeb4d3cc9eddbfbfc23d1cddf91dfc2a06e1355
Josh <jlockrow@gmail.com>
2019-01-20 17:57:54 -0600

    creates queue data structure from linkedList

--- src/QueueExample.java
+++ src/QueueExample.java
@@ -1,8 +1,32 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class QueueExample {
+public class QueueExample<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+
+    // constructor
+    public QueueExample(){}
+
+    public int size() {
+        return list.size();
+    }
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+    public void enqueue(E element) {
+        list.addLast(element);
+    }
+
+    public E dequeue() {
+        list.removeFirst();
+    }
+    public E first() {
+        return list.first();
+    }
+
+
+    // main
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+
     }
 }
